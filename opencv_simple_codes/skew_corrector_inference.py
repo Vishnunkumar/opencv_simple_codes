a64d882adb1dfe37691889863248790585a03642
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2022-02-05 13:11:06 +0530

    update

--- opencv_simple_codes/skew_corrector_inference.py
+++ opencv_simple_codes/skew_corrector_inference.py
@@ -8,3 +8,25 @@ import joblib
 class SkewCorrector:
   
   def __init__(self, model, scaler):
+
+    self.model = model
+    self.scaler = scaler
+    self.img_path = img_path
+
+  def skew_correction(self):
+
+    self.scaler = joblib.load('sscaler.gz')
+    self.model = tf.keras.models.load_model("scaled_model_b_50eps.h5")
+
+    imge = cv2.imread(self.img_path)
+    imge = cv2.resize(imge, (img_width, img_height))
+    imge = imge.reshape(1, img_height, img_width, 3)
+    imge = imge/255
+    angle = self.model.predict(imge)
+    rt_angle = self.scaler.inverse_transform(angle)[0][0]
+    rt_angle = round(rt_angle,0)
+
+    img = PIL.Image.open(fp)
+    img2 = img.rotate(rt_angle, expand=True, fillcolor="white")
+
+    return img2
